377946c7fe34a3de181a5c266c1d79d302ac9fb0
rezenman <rezenman@gmail.com>
2023-03-23 13:36:30 +0200

    added sample name to log file name

--- scripts/enrich_db.sh
+++ scripts/enrich_db.sh
@@ -8,7 +8,6 @@
 module load cd-hit/4.8.1
 
 clustering_id=0.99
-echo -e "-----------Creating sequences to enrich database-----------\n" > log.txt
 ## enlisting all arguments required to run the script
 while getopts 'c:hI:D:N:' OPTION; do
   case "$OPTION" in
@@ -43,30 +42,37 @@ then
     exit 1
 fi
 
+echo -e "-----------Creating sequences to enrich database-----------\n" > $sample_name"_log.txt"
 
-echo "Analyzing sample: $input" >> log.txt
-echo "Reference database: $db" >> log.txt
-echo "Sample name: $sample_name" >> log.txt
-echo "Clustering identity: $clustering_id" >> log.txt
+echo "Analyzing sample: $input" >> $sample_name"_log.txt"
+echo "Reference database: $db" >> $sample_name"_log.txt"
+echo "Sample name: $sample_name" >> $sample_name"_log.txt"
+echo "Clustering identity: $clustering_id" >> $sample_name"_log.txt"
 
 ##1 - Clustering each sample to remove redundant sequences
 
-echo -e "\n-----------Clustering sample to remove redundancy-----------\n" >> log.txt
+echo -e "\n-----------Clustering sample to remove redundancy-----------\n" >> $sample_name"_log.txt"
 
 out_file=$sample_name"_clustered.99.clst"
-cd-hit-est -i $input -o $out_file -c $clustering_id -n 10 -d 0 -M 0 -r 0 &>> log.txt
+cd-hit-est -i $input -o $out_file -c $clustering_id -n 10 -d 0 -M 0 -r 0 &>> $sample_name"_log.txt"
 
-if [ $? -eq 0 ] ; then echo -e "Finished clustering: $sample_name" >> log.txt ; else echo "Failed clustering $sample_name, check log" >>log.txt && exit  ; fi
+if [ $? -eq 0 ] ; then echo -e "Finished clustering: $sample_name" >> $sample_name"_log.txt" ; else echo "Failed clustering $sample_name, check log" >>$sample_name"_log.txt" && exit  ; fi
+
+#reformating cluster table to txt fromat
+clstr2txt.pl $sample_name"_clustered.99.clst.clstr" > $sample_name"_clustered.99.clst.clstr.txt" 
+
 
 
 ##2 - Clustering non-redundant sequences to enrich database with non matching sequences
-echo -e "\n-----------Clustering non-redundant sequences against the database-----------\n" >> log.txt
+echo -e "\n-----------Clustering non-redundant sequences against the database-----------\n" >> $sample_name"_log.txt"
 out_file_2d=$sample_name"_clustered2d_silva.fasta"
 cd-hit-est-2d \
     -i $db \
     -i2 $out_file \
-    -o $out_file_2d -c $clustering_id -n 10 -g 1 -d 0 -M 0 -r 0 -T 0 &>> log.txt
+    -o $out_file_2d -c $clustering_id -n 10 -g 1 -d 0 -M 0 -r 0 -T 0 &>> $sample_name"_log.txt"
 
-if [ $? -eq 0 ] ; then echo -e "Finished comparing: $sample_name to the database" >> log.txt ; else echo "Failed comparing $sample_name to the database, check log" >>log.txt && exit  ; fi
+if [ $? -eq 0 ] ; then echo -e "Finished comparing: $sample_name to the database" >> $sample_name"_log.txt" ; else echo "Failed comparing $sample_name to the database, check log" >>$sample_name"_log.txt" && exit  ; fi
+clstr2txt.pl $sample_name"_clustered.99.clst.clstr" > $sample_name"_clustered2d_silva.fasta.clst.clstr.txt"
 
-echo -e "All non-redundant sequences not clustered with the database are found in the file: $out_file_2d" >> log.txt
+
+echo -e "All non-redundant sequences not clustered with the database are found in the file: $out_file_2d" >> $sample_name"_log.txt"
